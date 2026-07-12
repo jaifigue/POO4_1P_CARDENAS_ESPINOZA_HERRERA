83acfdafaec2dc83c3e449c3109e8d8260701f8d
Jair Cardenas <jaifigue@espol.edu.ec>
2026-07-12 15:18:12 -0500

    Agrega clase Organizador

--- src/Organizador.java
+++ src/Organizador.java
@@ -1,19 +1,35 @@
 package src;
+import java.util.ArrayList;
 
-public class Organizador extends Usuario {
+public class Organizador extends Usuario{
     private String empresa;
     private String cargo;
 
-    public Organizador() {
+    public Organizador(String codigoUnico, String cedula, String nombres, String apellidos, String usuario, String contrasena,
+        String correo, String empresa, String cargo){
+            super(codigoUnico, cedula, nombres, apellidos, usuario, contrasena, correo, RolUsuario.O);
+            this.empresa = empresa;
+            this.cargo = cargo;
+        }
+    
+    @Override
+    public void consultarEntradas(ArrayList<Compra> compras){
+        if(compras ==null || compras.isEmpty()){
+            System.out.println("No hay compras registradas");
+        }
+        else {
+            for(Compra c : compras){
+                System.out.println(c);
+            }
+        }
     }
 
-    public Organizador(String codigoUnico, String cedula, String nombres, String apellidos,
-                       String usuario, String contrasena, String correo,
-                       String empresa, String cargo) {
-        super(codigoUnico, cedula, nombres, apellidos, usuario, contrasena, correo, RolUsuario.O);
-        this.empresa = empresa;
-        this.cargo = cargo;
+    @Override
+    public String toString(){
+        return "Codigo único: "+codigoUnico+"\nCédula: "+cedula+"\nNombres: "+nombres+"\nApellidos: "+apellidos+"\nEmpresa: "+empresa
+        +"\nCargo: "+cargo;
     }
+
 
     public String getEmpresa() {
         return empresa;
@@ -30,4 +46,6 @@ public class Organizador extends Usuario {
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
+
+    
 }
